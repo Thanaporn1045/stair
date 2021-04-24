775ada9f78853060100ab6332950cc2ebb27693e
Thanaporn1045 <78547225+Thanaporn1045@users.noreply.github.com>
2021-04-24 16:04:13 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace สอบกลางภาควันที่24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int width, height, num,i,j,k;
+            a = Console.ReadLine();
+            width = int.Parse(Console.ReadLine());
+            height = int.Parse(Console.ReadLine());
+            num = int.Parse(Console.ReadLine());
+
+            for (k = 1; k <= num; k++)
+            {
+                for (i = 1; i <= height ; i++)
+                {
+                    for (j = 1; j <= width *k; j++)
+                    { Console.Write(a); }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
